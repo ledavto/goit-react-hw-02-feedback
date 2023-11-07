f665b2a5dd9044f71d3994efa0e8db5feb2b8980
ledavto <ser_ega@ukr.net>
2023-11-07 15:54:52 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Counter extends Component {
+  static defaultProps = {
+    step: 1,
+    initialValue: 0,
+  };
+
+  state = {
+    value: this.props.initialValue,
+  };
+
+  handleIncrement = evt => {
+    console.log('Increment button was clicked!', evt); // працює
+    console.log('this.props: ', this.props); // працює
+  };
+
+  handleDecrement = evt => {
+    console.log('Decrement button was clicked!', evt); // працює
+    console.log('this.props: ', this.props); // працює
+  };
+
+  render() {
+    const { step } = this.props;
+
+    return (
+      <div>
+        <span>{this.state.value}</span>
+        <button type="button" onClick={this.handleIncrement}>
+          Increment by {step}
+        </button>
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement by {step}
+        </button>
+      </div>
+    );
+  }
+}
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Counter step={5} />
     </div>
   );
 };
